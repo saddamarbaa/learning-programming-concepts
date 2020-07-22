bb3a256393204b51ba105d938064b60317618bf2
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-23 03:07:38 +0700

    C++ Program that take User Input

--- Input_Output_endl.cpp
+++ Input_Output_endl.cpp
@@ -2,7 +2,7 @@
      [AUTHOR]: Saddam Arbaa
      [Email] : <saddamarbaas@gmail.com>
     
-     C++ Input/Output/endl  */
+     C++ Program that take User Input  */
     
 
 #include <iostream>
@@ -11,14 +11,24 @@ using namespace std;
 
 int main()
 {
-    
+    // char variable
     char a;
+    
+    //declare integer variable
     int num;
+     
+    // declare double variable
     double num2 = 256.783;
+     
+    // Taking input from user
     cout << "Enter a character and an integer: ";
     cin >> a >> num; // Taking input from user
+     
     cout << "Character: " << a << endl;  // print char
+     
     cout << "Number: " << num  << endl; // print integer
+     
     cout << "double: " << num2 << endl;  // print double
+     
     return 0;
 }

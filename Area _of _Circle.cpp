83df81b0c4c33b707082458398a78a9ce39d68ac
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-23 03:27:04 +0700

    Area  of Circle

---
+++ Area _of _Circle.cpp
@@ -0,0 +1,33 @@
+/*
+ [AUTHOR]:       Saddam Arbaa
+ [Email] :      <saddamarbaas@gmail.com>
+
+               C++ program Circle of Area */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int radius;
+    float area;
+    const float  PI = 3.14;
+
+    // asking user to enter Radius
+    cout<< "Enter the Radius :" ;
+    cin >> radius;
+
+    // calculating the area
+    area = PI * radius * radius;
+
+    /*
+      also can be written like this
+      area = PI * pow(radius ,2);
+      if so we will need call cmath header file
+    */
+
+    // printing the area
+    cout << "the area of the circle is : " << area <<endl;
+    return 0;
+}

0cdc495868c8c8fca1e86decf6e35c4306861bd3
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-23 03:19:40 +0700

    C++ program  the average of 5 Numbers

--- average.cpp
+++ average.cpp
@@ -10,24 +10,28 @@ using namespace std;
 
 int main()
 {
+    /* declare 5 variable to store the 5 numbers and sum variable 
+       to store the sum and average variable to store the average 
+       of all numbers */
+  
     int num1,num2,num3,num4,num5,sum,average;
 
     // asking user to enter the numbers
-    cout<<"Enter five Real number to find the sum and Average\n";
-    cout<<"Enter the first Real number :";
-    cin>> num1;
+    cout << "Enter five Real number to find the sum and Average\n";
+    cout << "Enter the first Real number :";
+    cin >> num1;
 
-    cout<<"Enter the second Real number :";
-    cin>> num2;
+    cout << "Enter the second Real number :";
+    cin >> num2;
 
-    cout<<"Enter the third Real number  :";
-    cin>> num3;
+    cout << "Enter the third Real number  :";
+    cin >> num3;
 
-    cout<<"Enter the forth Real number  :";
-    cin>> num4;
+    cout << "Enter the forth Real number  :";
+    cin >> num4;
 
-    cout<<"Enter the fifth Real number  :";
-    cin>> num5;
+    cout << "Enter the fifth Real number  :";
+    cin >> num5;
 
     // the sum
     sum = num1 + num2 + num3 + num4 + num5 ;

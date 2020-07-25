e53847cc526f66b06b4fdeab14fdc6e1b8a0ef0f
saddamarbaa <saddamarbaas@gmail.com>
2020-07-26 04:38:54 +0700

    C++ Data Types

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -5,7 +5,6 @@
 
    [AUTHOR]		: Saddam Arbaa <saddamarbaas@gmail.com>  */
 
-
 #include <iostream>
 
 #include <string>
@@ -13,31 +12,32 @@
 using namespace std;
 
 int main ()
- {
-  // declare Integer variable
-  int number = 15;               // Integer (whole number)
+{
+    // declare Integer variable
+    int number = 15;               // Integer (whole number)
 
-  // declare Float type variable
-  float floatNmber = 13.56;     // Floating point number
+    // declare Float type variable
+    float floatNmber = 13.56;     // Floating point number
 
-  // declare Float type variable
-  double myDoubleNumber = 20.58;   // Floating point number
+    // declare Float type variable
+    double myDoubleNumber = 20.58;   // Floating point number
 
-  // declare Character type variabl
-  char letter = 'A';         // Character
+    // declare Character type variable
+    char letter = 'A';         // Character
 
-  // declare Boolean type variabl
-  bool boolean = false;       // Boolean
-  // declare String type variabl
-  string name = "saddam";   // String
+    // declare Boolean type variable
+    bool boolean = false;       // Boolean
 
-  // Print variable values
-  cout << "int: " << number << "\n";
-  cout << "float: " << floatNmber << "\n";
-  cout << "double: " << myDoubleNumber << "\n";
-  cout << "char: " << letter << "\n";
-  cout << "bool: " << boolean << "\n";
-  cout << "string: " << name << "\n";
+    // declare String type variable
+    string name = "my name is saddam";   // String
+
+    // Print variable values
+    cout << "int : " << number <<"\n";
+    cout << "float : " << floatNmber << "\n";
+    cout << "double : " << myDoubleNumber << "\n";
+    cout << "char : " << letter << "\n";
+    cout << "boolean: " << boolean << "\n";
+    cout << "string: " << name << "\n";
 
   return 0;
 }

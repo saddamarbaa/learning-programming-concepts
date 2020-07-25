5bb4a8afaf54b7d9227da8463c65663322d9eaeb
saddamarbaa <saddamarbaas@gmail.com>
2020-07-26 04:27:40 +0700

    my commit

---
+++ DataTypes.cpp
@@ -0,0 +1,43 @@
+/**
+                : C++ Data Types
+
+   [PROG]	    : Union by Rank implementation
+
+   [AUTHOR]		: Saddam Arbaa <saddamarbaas@gmail.com>  */
+
+
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main ()
+ {
+  // declare Integer variable
+  int number = 15;               // Integer (whole number)
+
+  // declare Float type variable
+  float floatNmber = 13.56;     // Floating point number
+
+  // declare Float type variable
+  double myDoubleNumber = 20.58;   // Floating point number
+
+  // declare Character type variabl
+  char letter = 'A';         // Character
+
+  // declare Boolean type variabl
+  bool boolean = false;       // Boolean
+  // declare String type variabl
+  string name = "saddam";   // String
+
+  // Print variable values
+  cout << "int: " << number << "\n";
+  cout << "float: " << floatNmber << "\n";
+  cout << "double: " << myDoubleNumber << "\n";
+  cout << "char: " << letter << "\n";
+  cout << "bool: " << boolean << "\n";
+  cout << "string: " << name << "\n";
+
+  return 0;
+}

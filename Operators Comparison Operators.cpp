c4198619185053bd673b2ef565eaf7cde2a115d9
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-26 05:35:48 +0700

    Comparison Operators  in c++

---
+++ Operators Comparison Operators.cpp
@@ -0,0 +1,37 @@
+/**
+                :  Comparison Operators  in c++
+
+   [PROG]	    : Comparison Operators
+
+   [AUTHOR]		: Saddam Arbaa <saddamarbaas@gmail.com>  */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    int y = 3;
+
+    //  ==   Equal to
+    cout << (x == y)<<endl; // returns 0 (false) because 5 is not equal to 3
+
+    //  !=   Not equal to
+    cout << (x != y)<<endl; // returns 1 (true) because 5 is not equal to 3
+
+    // >  Greater than
+    cout << (x > y)<<endl; // returns 1 (true) because 5 is greater than 3
+
+    // < Less than
+    cout << (x < y)<<endl; // returns 0 (false) because 5 is not less than 3
+
+    // >=   Greater than or equal to
+    cout << (x >= y)<<endl; // returns 1 (true) because five is greater than, or equal, to 3
+
+    // <=   Less than or equal to
+    cout << (x <= y)<<endl; // returns 0 (false) because 5 is neither less than or equal to 3
+
+    return 0;
+}

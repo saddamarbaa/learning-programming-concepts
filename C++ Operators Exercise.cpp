baa54d298b2d3aa67daa8204e99413f38da68f7d
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-27 05:02:41 +0700

    C++  Exercise  on Operators
    C++  Exercise  on Operators

--- C++ Operators Exercise.cpp
+++ C++ Operators Exercise.cpp
@@ -1,1 +1,24 @@
 
+/**
+   [PROG]	     : C++ / (Exercise) Operators
+   [AUTHOR]		 : Saddam Arbaa <saddamarbaas@gmail.com>  */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x =11;
+    int m = (6>1) | (6^16)&x++;
+
+    /* (1) m = (6>1) | (6^16)&11;
+       (2) m = (6>1) | 22 &11;
+       (3) m = (6>1) | 2;
+       (4) m = 1 | 2;
+       (5) m = 3
+       */
+    // result 3
+    cout << m;
+    return 0;
+}

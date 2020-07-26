f95d208de53ac4064a9d1febd4ce8f3626caf614
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-27 05:10:14 +0700

    C++  Exercise  on Operators
    C++  Exercise  on Operators

--- C++ Operators Exercises..cpp
+++ C++ Operators Exercises..cpp
@@ -1,1 +1,27 @@
 
+/**
+   [PROG]	    : C++  Exercise  on Operators
+
+   [AUTHOR]		: Saddam Arbaa <saddamarbaas@gmail.com>  */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 10 ;
+    int m = (6 <1 ) | (20 ^ 16) & x++ + 1;
+
+    /*
+    m = F | 4 & 10 + 1
+    m = F | 4 & 11
+    m = F | 0
+    m = 0 | 0
+    m = 0
+    */
+
+    // result is 0
+    cout << m << endl;
+    return 0;
+}

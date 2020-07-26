e042d42d35fe8cc24107bcc1a2b4a5beece6d1b8
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-27 04:55:54 +0700

    C++  Logical  Operators
    C++  Logical  Operators

--- C++ Operators Logical Operators.cpp
+++ C++ Operators Logical Operators.cpp
@@ -1,1 +1,26 @@
 
+/**
+   [PROG]	    : Logical Operators in c++
+
+   [AUTHOR]		: Saddam Arbaa <saddamarbaas@gmail.com>  */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    int y = 3;
+
+    // && and Logical operator 	Returns true if both statements are true
+    cout << (x > 3 && x < 10)<<endl; // returns true (1) because 5 is greater than 3 AND 5 is less than 10
+
+    // || or Logical Logical operator 	Returns true if one of the statements is true
+    cout << (x > 3 || x < 4)<<endl; // returns true (1) because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
+
+    // ! not Logical  operator Reverse the result, returns false if the result is true
+    cout << (!(x > 3 && x < 10))<<endl; // returns false (0) because ! (not) is used to reverse the result
+
+    return 0;
+}

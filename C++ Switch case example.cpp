31a20db210ae98920159508875cabe289dea8e92
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-29 03:04:11 +0700

    C++ Switch case More  Example
    C++ Switch case example

---
+++ C++ Switch case example.cpp
@@ -0,0 +1,33 @@
+/*
+   [AUTHOR]: Saddam Arbaa
+
+   [Email] : <saddamarbaas@gmail.com>
+
+   C++ switch example
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int i=2;
+   switch(i)
+   {
+      case 1:
+        cout<<"Case1 "<<endl;
+        break;
+      case 2:
+        cout<<"Case2 "<<endl;
+        break;
+      case 3:
+        cout<<"Case3 "<<endl;
+        break;
+      case 4:
+        cout<<"Case4 "<<endl;
+        break;
+      default:
+        cout<<"Default "<<endl;
+    }
+    return 0;
+}

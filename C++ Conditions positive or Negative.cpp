bbbedd88a5298024caac93b8fa18079867da03b9
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-07-29 02:31:00 +0700

    C++ program positive or Negative number
    C++ program positive or Negative number

--- C++ Conditions positive or Negative.cpp
+++ C++ Conditions positive or Negative.cpp
@@ -1,1 +1,30 @@
 
+/*
+ simple if - else statement
+ write program that accept an integer from the user and
+ print out whether it is positive or Negative number
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+
+    // asking number from user
+    cout << "enter number !  :" ;
+    cin >> number;
+
+    // checking condition
+    if (number > 0)
+    {
+        cout << "number " <<  number <<" : is positive number \n";
+    }
+    else
+    {
+        cout << "number " <<  number <<" : is positive number \n";
+    }
+}

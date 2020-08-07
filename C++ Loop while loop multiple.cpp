89fca7ef572abede784e91ad8c6a598a523cbf29
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-08 03:24:26 +0700

    While Loop program to print multiple of number from 1 to 10

---
+++ C++ Loop while loop multiple.cpp
@@ -0,0 +1,31 @@
+/**
+[AUTHOR]: Saddam Arbaa
+[Email] : <saddamarbaas@gmail.com>
+
+  while loop example
+
+  c++ program to print out multiple of number from 1 to 10
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    int i = 1; // counter
+    int fac = 1; // variable for multiplying
+
+    while (i <= 10)
+    {
+        fac *= i; // fac = fac * 1 //calculation fac
+        i = i +1; // increment the counter
+
+    }
+    cout << "The multiple of number from 1 to 10 is: " << fac << endl;
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */
+

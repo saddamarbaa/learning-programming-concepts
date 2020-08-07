22a46e91bd442d2a4f44cea7a941bc93f9c8195c
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-08 01:55:56 +0700

    C++ Do/While Loop print number from 100 to 2 in descending order
    C++ Do/While Loop print number from 100 to 2 in descending order

---
+++ C++ Loop do while loop .cpp
@@ -0,0 +1,31 @@
+/**
+[AUTHOR]: Saddam Arbaa
+[Email] : <saddamarbaas@gmail.com>
+
+ do while loop examples in C++
+ write c++ program that out number from 100 to 2
+ in descending order
+*/
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    int i = 100; // counter start from 100
+    do
+    {
+        // print numbers from 100 to 2
+        cout << i << "\n";
+
+        i--; // i = i -1 // decrement counter
+
+    }  while (i >= 2);
+
+    // add end line after loop
+    cout << endl;
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */

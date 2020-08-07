6addabc4750e638108d00308ac014e8773cd9e48
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-08 01:38:56 +0700

    C++ program to demonstrate Infinitive do-while loop
    C++ program to demonstrate Infinitive do-while loop

---
+++ C++ Loop infinite do while loops.cpp
@@ -0,0 +1,25 @@
+/**
+    [AUTHOR]: Saddam Arbaa
+    [Email] : <saddamarbaas@gmail.com>
+
+    C++ Infinitive do-while Loop
+    C++ program to demonstrate Infinitive do-while
+    this program will run forever */
+
+
+#include <iostream>
+
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    do
+    {
+        cout << "Hello !" << endl;
+        cout<<"Infinitive do-while Loop";
+    } while(true);
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */

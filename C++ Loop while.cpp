8dcaea0d5dc5c6d2a158975357499080ca07202e
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-08 03:03:44 +0700

    C++ While Loop program print number from 100 to 2

---
+++ C++ Loop while.cpp
@@ -0,0 +1,29 @@
+/**
+[AUTHOR]: Saddam Arbaa
+[Email] : <saddamarbaas@gmail.com>
+
+ while loop examples in C++
+ write c++ program that out number from 100 to 2
+ in descending order
+*/
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    int i = 100; // counter start from 100
+    while (i >= 2)
+    {
+        // print numbers from 100 to 2
+        cout << i << "\n";
+
+        i--; // i = i -1 // decrement counter
+    }
+    // add end line after loop
+    cout << endl;
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */

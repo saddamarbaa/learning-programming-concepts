c77f8ebbbc8f466692aa42b4c4cb8ac75324ca40
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-08 02:14:54 +0700

    C++ program to demonstrate while do loop print star
    C++ program to demonstrate while do loop print star

---
+++ C++ Loop do while print star.cpp
@@ -0,0 +1,33 @@
+/**
+[AUTHOR]: Saddam Arbaa
+[Email] : <saddamarbaas@gmail.com>
+
+  do while loop example
+  c++ program ask user to enter number  then as long
+  as user input bigger than 0 prints "**" and ask
+  for anther number
+*/
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    int a;
+
+    // asking user input
+    cout << "Enter number :";
+    cin >> a;
+   do
+    {
+        cout << "* * \n";
+
+        // asking user input again and again
+        cout << "Enter another number :";
+        cin >> a;
+    } while (a > 0);
+
+   return 0;// signal to operating system everything works fine
+
+}/** End of main function */

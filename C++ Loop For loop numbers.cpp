c247fc1f43153494adde537505b7161533016355
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-08 13:30:16 +0700

    C++ for Loop example print number from 20 to 2
    C++ for Loop  example print sum of number from 20 to 2

---
+++ C++ Loop For loop numbers.cpp
@@ -0,0 +1,26 @@
+/**
+[AUTHOR]: Saddam Arbaa
+[Email] : <saddamarbaas@gmail.com>
+
+ for loop examples in C++
+ write c++ program that out number from 2 to 2
+ in descending order
+ N will be entered by user
+*/
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    for(int i = 20; i >= 2; i--)
+    {
+        // print numbers from 20 to 2
+        cout << i << "\n";
+    }
+    // add end line after loop
+    cout << endl;
+     return 0;// signal to operating system everything works fine
+
+}/** End of main function */

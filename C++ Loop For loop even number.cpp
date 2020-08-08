1ed36047545670e2c42452e59e5e8173c78c8f4e
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-08 13:39:45 +0700

    C++ for Loop program to print even numbers between 2 to 20

---
+++ C++ Loop For loop even number.cpp
@@ -0,0 +1,27 @@
+/**
+[AUTHOR]: Saddam Arbaa
+[Email] : <saddamarbaas@gmail.com>
+
+ for loop examples in C++
+ This example will  print even numbers between 2 to 20
+*/
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+
+    for(int i = 2; i <= 20; i = i + 2)  // i+=2 == i = i +2;
+    {
+        // print eve numbers from 2 to 20 including 20 its self
+        cout << i << "\t";
+    }
+
+    // add end line after loop
+    cout << endl;
+
+   return 0;// signal to operating system everything works fine
+
+}/** End of main function */

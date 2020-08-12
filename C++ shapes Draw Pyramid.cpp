d53b30aa518a9cb60315d9d941d7522791b2b253
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-13 02:54:14 +0700

    C++ program Draw Pyramid using nested loops and pattern
     C++ program Draw Pyramid using nested loops and pattern

---
+++ C++ shapes Draw Pyramid.cpp
@@ -0,0 +1,52 @@
+/**
+    [AUTHOR]: Saddam Arbaa
+    [Email] : <saddamarbaas@gmail.com>
+
+    C++  Loop Example to Draw  Pyramid  like the one blow
+
+                  *
+                * * *
+              * * * * *
+            * * * * * * *
+          * * * * * * * * *
+        * * * * * * * * * * *
+      * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * *
+  * * * * * * * * * * * * * * * * *
+* * * * * * * * * * * * * * * * * * *
+
+number of row will entered by user (user entered 10) */
+
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    int rows; //variable for row
+    // asking user input to enter number of rows
+    cout << "Enter number of rows :";
+    cin >> rows;
+
+    // outer loop need 3 nested loop
+    for(int i = 1; i <= rows; i++)
+    {
+        // first inner loop
+        for(int k = 1; k <= rows - i; k++)
+        {
+           cout << "  " ; // print spaces
+        }
+
+      // second inner loop
+        for(int j = 1; j <= (2 * i - 1 ) ; j++)
+        {
+          cout << "* " ; // print stars *
+        }
+
+         cout << endl; // new line after each inner loop
+    }
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function  */

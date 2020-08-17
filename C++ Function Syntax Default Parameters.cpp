04b23088c3a089978adb6a7ee8750606e86c9fc7
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-17 22:19:08 +0700

    C++ Functions // Default Parameter Value
    C++ Functions // Default Parameter Value

--- C++ Function Syntax Default Parameters.cpp
+++ C++ Function Syntax Default Parameters.cpp
@@ -1,1 +1,49 @@
+/**
+    [PROGRAM] : C++ Default Parameters
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
+    C++ function with  Default Parameters  */
+
+#include <iostream>
+using namespace std;
+
+/**
+  Function definition / function  with Default Parameter Value
+  this function print the given country name but if the user
+  don't pass any parameter will print the default value sudden*/
+
+void myFunction(string country = "Sudan")
+{
+    cout << country << "\n";
+}
+
+// the main Function
+int main()
+{
+    // call the function 8 time
+    myFunction("Indonesia");
+    myFunction("South Africa");
+    myFunction(); // will print the Default Parameter
+    myFunction("India");
+    myFunction();  // will print the Default Parameter
+    myFunction("Sweden");
+    myFunction("USA");
+    myFunction("china");
+
+    // out put will be as blow
+     // Indonesia
+     // South Africa
+     // sudan
+     // India
+     // sudan
+     // Sweden
+     // USA
+     //china
+
+     return 0;// signal to operating system everything works fine
+
+}/** End of main function */
+
+
+
 

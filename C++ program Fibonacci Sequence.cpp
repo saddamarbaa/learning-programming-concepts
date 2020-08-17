c5938b61da4151efddbef7205f7fb7c955278cf9
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-17 19:05:57 +0700

    This C++ Program Print Fibonacci Sequence from 1  to N.

--- C++ program Fibonacci Sequence.cpp
+++ C++ program Fibonacci Sequence.cpp
@@ -1,1 +1,45 @@
 
+/**
+    [PROGRAM] : C++ / (Exercise) Fibonacci Sequence
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
+
+   write C++  program to print Fibonacci Sequence from zero to N
+   using loop  */
+
+#include <iostream>
+using namespace std;
+
+// the  Driver Code
+int main()
+{
+    /* variables declaration */
+    int i, a, b, c, n;
+
+    // get valid number
+    do
+    {
+        // asking valid positive number from user
+        cout << "Enter N --> N must be bigger than zero : ";
+        cin >> n;
+    }while(n <= 0);
+
+    a = 0; // initialize a by zero
+    b = 1; // initialize b by one
+
+    //  print first 2 Fibonacci number then loop for others
+    cout << a << "  " << b << "  ";
+
+    /**  Calculating  Fibonacci Sequence  start from here*/
+    for( i = 1; i <= n - 1; i++)
+    {
+        c = a + b; // Calculate sum of a+ b and store in c
+        cout << c  << "  ";
+        a = b; // store in a value of b
+        b = c; // store in b value of c which sum of (a+b)
+
+    } /* End of loop */
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */

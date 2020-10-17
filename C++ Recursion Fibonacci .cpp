cc14db27139201476febc96b7c52b66691d80f51
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-10-18 01:25:44 +0700

    C++  Recursion // Fibonacci Sequence number > N

--- C++ Recursion Fibonacci .cpp
+++ C++ Recursion Fibonacci .cpp
@@ -1,1 +1,39 @@
+/**
+    [PROGRAM] : C++ / (Exercise) Fibonacci Sequence
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com> */
 
+#include <iostream>
+using namespace std;
+
+// Function declaration
+int fib(int n);
+
+int main() // the  Driver Code
+{
+    int n, result; /* variables declaration */
+    do // get valid number
+    {
+       // asking valid positive number from user
+      cout << "Enter N --> N must be bigger than zero : ";
+      cin >> n;
+    }while(n <= 0);
+    result = fib(n);    // call function
+    cout << result<<endl; // print the result
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */
+
+/** Recursive function to calculate Fibonacci Sequence
+    assuming that n is positive integer
+ */
+
+int fib(int n)
+{
+    if(n <= 1) // base case
+    {
+        return n;
+    }
+    // Recursive Case
+    return fib(n - 1) + fib(n - 2);
+}
